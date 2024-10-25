5e8c86fcf075f0b76ec10ce801b9993d2fce3182
JulianSandoval25 <juliansandoval1996@gmail.com>
2024-10-24 23:24:23 -0300

    add panel users

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :users
   resources :categories
   resources :products
   resources :product_histories
